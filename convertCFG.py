9c2062d814219d15a351e1db795f6dc871b10855
Fajar Herawan <13521080@std.stei.itb.ac.id>
2022-11-25 10:35:44 +0700

    CFGtoCNF

--- convertCFG.py
+++ convertCFG.py
@@ -3,14 +3,15 @@ def loadGrammar(filePath):
     Terminal = file.split("Variables:\n")[0].replace("Terminals:\n","").replace("\n","")
     Variabel = file.split("Variables:\n")[1].split("Productions:\n")[0].replace("\n","")
     Productions = file.split("Productions:\n")[1].replace("\n","").split(";")
+    Variabel = Variabel.split(" ")
+    Terminal = Terminal.split(" ")
     fixProductions = []
     for i in Productions:
-        temp = i.replace(" ","").split("->")
-        array = []
-        rule = temp[1].split("|")
-        fixProductions.append([temp[0],rule])
+        var = i.split(" -> ")[0].replace(" ","")
+        term = i.split(" -> ")[1].split(" | ")
+        for j in term:
+            fixProductions.append([var,j.split(" ")])
     return Terminal, Variabel, fixProductions
-(a,b,c) = loadGrammar("test.txt")
 
 def setofOneTerminal(productions, variables, terminal):
     set = {}
@@ -20,27 +21,27 @@ def setofOneTerminal(productions, variables, terminal):
     return set
 
 def subtitutedRule(rules, variables):
-	unitaryList, simplified_rules = [], []
-	for rule in rules:
-		if rule[0] in variables and rule[1][0] in variables and len(rule[1]) == 1 :
-			unitaryList.append( [rule[0], rule[1][0]] )
-		else:
-			simplified_rules.append(rule)
-	for ruleUnit in unitaryList:
-		for rule in rules:
-			if ruleUnit[1]==rule[0] and ruleUnit[0]!=rule[0]:
-				simplified_rules.append( [ruleUnit[0],rule[1]])
-    return simplified_rules
+    unitaryRule, nonUnitaryRule = [], []
+    for rule in rules:
+        if len(rule[1]) == 1 and rule[1][0] in variables:
+            unitaryRule.append([rule[0],rule[1][0]])
+        else:
+            nonUnitaryRule.append(rule)
+    for rule in unitaryRule:
+        for rule2 in rules:
+            if rule[1] == rule2[0] and rule2[0]!=rule[0]:
+                nonUnitaryRule.append([rule[0],rule2[1]])
+    return nonUnitaryRule
 
 def ConvertToCNF(productions, variables, terminal):
-    variables.append("S'")
-    productions = [['S',[variables[0]]]] + productions
-    new_Productions = []
+    variables.append('S0')
+    productions = [['S0',[variables[0]]]] + productions
+    temp = []
     setofOneProductions = setofOneTerminal(productions, variables, terminal)
     index = 0
     for rule in productions: # Mengganti rule yang memuat terminal dan Variabel menjadi Variabel semua
         if rule[0] in variables and rule[1][0] in terminal and len(rule[1]) == 1:
-            new_Productions.append(rule)
+            temp.append(rule)
         else:
             for j in terminal:
                 for i,value in enumerate(rule[1]):
@@ -48,37 +49,48 @@ def ConvertToCNF(productions, variables, terminal):
                         index = index+1
                         setofOneProductions[j] = "A"+str(index)
                         variables.append(setofOneProductions[j])
-                        new_Productions.append([setofOneProductions[j],[j]])
+                        temp.append(setofOneProductions[j],[j])
                         rule[1][i] = setofOneProductions[j]
                     elif j == value :
                         rule[1][i] = setofOneProductions[j]
-            new_Productions.append([rule])
-    productions = new_Productions #Menghapus non-unit production
-    new_Productions = []
+            temp.append(rule)
+    productions = temp #Menghapus non-unit production
+    temp = []
     index = 0
     for rule in productions:
         if len(rule[1]) <= 2:
-            new_Productions.append(rule)
+            temp.append(rule)
         else:
             index = index+1
             VarName = "B"+str(index)
             variables.append(VarName+"1")
-            new_Productions.append([rule[0],[rule[1][0]]+[VarName+"1"]])
+            temp.append([rule[0],[rule[1][0]]+[VarName+"1"]])
+            i = 1
             for i in range(1,len(rule[1])-2):
                 var1,var2 = VarName+str(i),VarName+str(i+1)
                 variables.append(var2)
-                new_Productions.append([var1,[rule[1][i],var2]])
-            new_Productions.append([VarName+str(len(rule[1])-1),rule[1][len(rule[1])-2:len(rule[1])]])
-    productions = new_Productions #Menghapus non-unit production
+                print([var1,[rule[1][i],var2]])
+                temp.append([var1,[rule[1][i],var2]])
+    productions = temp 
     j = 0
+    result = []
+    result = subtitutedRule(productions, variables)
+    temp = subtitutedRule(result, variables)
+    while temp != result and j < 10:
+        result = subtitutedRule(temp, variables)
+        temp = subtitutedRule(result, variables)
+        j = j+1
+    return result
 
-
-
-
-
-
-    
-
-
-
-
+def saveCNF(productions):
+    file = open("CNF.txt","w")
+    temp = {}
+    for rule in productions:
+        if rule[0] in temp:
+            temp[rule[0]] += " | "+" ".join(rule[1])
+        else:
+            temp[rule[0]] = " ".join(rule[1])
+    Grammar = ""
+    for key,value in temp.items():
+        Grammar += key+" -> "+value+"\n"
+    file.write(Grammar)
