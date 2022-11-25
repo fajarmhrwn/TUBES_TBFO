d617ed4825156ba230b4510e403b2853d1c21a4d
Fajar Herawan <13521080@std.stei.itb.ac.id>
2022-11-25 15:52:39 +0700

    fix fa

--- coba.js
+++ coba.js
@@ -5,7 +5,9 @@ function a(
 }
 
 
-for(let i=0 ;i<10;i++){
+for(let i=0 ;
+    i<10;i++
+    ){
     console.log(i)
     break
     break 
--- fa.py
+++ fa.py
@@ -1,23 +1,73 @@
-# isinya fa masih kosongan
+def state1(cc):
+    '''State 1'''
+    if cc == '_' or cc.isalpha() or cc == '$':
+        return 2
+    elif cc == '.':
+        return 3
+    else :
+        return 3
+
+def state2(cc):
+    '''state 2'''
+    if cc == '_' or cc.isalpha() or cc == '$' or cc.isalnum():
+        return 2
+    elif cc == '.':
+        return 6
+    else:
+        return 3
+    
+def state3(cc):
+    '''state 3'''
+    return 3
+
+def state4(cc):
+    '''State 4'''
+    if cc.isnumeric():
+        return 4
+    else:
+        return 5
+    
+def state5(cc):
+    '''State 5'''
+    return 5
+
+def state6(nama):
+    '''State 6'''
+    jsKey = ['await','break','case','catch','class','const','continue','debugger','default'	,'delete', 'do'	,'else'	,'enum'	,'export','extends','false','finally','for','function','if','implements','import','in','instanceof','interface','let','new','null','package','private','protected','public','return','super','switch','static','this','throw','try','true','typeof','var','void','while','with','yield']
+    if nama in jsKey:
+        return 3
+    else:
+        return 6
+
+
+
 def cekVar(name):
     '''Check if it is a variable name'''
-    jsKey = ['await','break','case','catch','class','const','continue','debugger','default'	,'delete', 'do'	,'else'	,'enum'	,'export','extends','false','finally','for','function','if','implements','import','in','instanceof','interface','let','new','null','package','private','protected','public','return','super','switch','static','this','throw','try','true','typeof','var','void','while','with','yield']
-    if name in jsKey:
-        return False
+    name = name+'.'
+    state = 1
+    for i in name:
+        if state == 1:
+            state = state1(i)
+        if state == 2:
+            state = state2(i)
+        if state == 3:
+            state = state3(i)
+        if state == 6:
+            state = state6(name[:-1])
+    if state == 6:
+        return True
     else:
-        if name[0] != '_' or not(name[0].isalpha()) or name[0] != '$':
-            return False
-        else:
-            state = False
-            for i in name[1:]:
-                if i.isalnum() or i == '_' or i == '$' or i.isalpha():
-                    state = True
-            return state
-
+        return False
 
 def cekNum(num):
     '''Check if it is a number'''
+    state = 4
     for i in num:
-        if i.isdigit():
-            return True
-    return False
+        if state == 4:
+            state = state4(i)
+        if state == 5:
+            state = state5(i)
+    if state == 5:
+        return False
+    else:
+        return True
