a1da6b22546268e7a599fbd59441e3b76258f447
auliamey <auliaannandya@gmail.com>
2022-11-25 12:34:33 +0700

    splitter

--- fa.py
+++ fa.py
@@ -1,1 +1,74 @@
 # isinya fa masih kosongan
+def state1(c):
+    if(ord(c) >= 65 and ord(c) <= 90):
+        state = 2
+    elif(ord(c) == 95):
+        state = 2
+    elif(ord(c) >= 97 and ord(c) <= 122):
+        state = 2
+    else :
+        state = 3
+    return state
+
+def state2(c):
+    if(ord(c) >= 65 and ord(c) <= 90):
+        state = 2
+    elif(ord(c) == 95):
+        state = 2
+    elif(ord(c) >= 97 and ord(c) <= 122):
+        state = 2
+    elif(ord(c) >= 48 and ord(c) <= 57):
+        state = 2
+    else :
+        state = 3
+    return state
+
+def state3(c):
+    state = 3
+    return state
+
+def state4(c):
+    if(ord(c) >= 48 and ord(c) <= 57):
+        state = 5
+    else :
+        state = 6
+    return state
+
+def state5(c):
+    if(ord(c) >= 48 and ord(c) <= 57):
+        state = 5
+    else :
+        state = 6
+    return state
+
+def state6(c):
+    state = 6
+    return state
+
+def isVariable(s):
+    state = 1
+    for i in range (len(s)):
+        if (state == 1):
+            state = state1(s[i])
+        if (state == 2):
+            state = state2(s[i])
+        if (state == 3):
+            state = state3(s[i])
+    if (state == 2):
+        return True
+    else :
+        return False
+
+def isNumber(s):
+    state = 4
+    for i in range (len(s)):
+        if (state == 4):
+            state = state4(s[i])
+        if (state == 5):
+            state = state5(s[i])
+        if (state == 6):
+            state = state6(s[i])
+    if (state == 5):
+        return True
+    else :
+        return False
---
+++ tes.py
@@ -0,0 +1,7 @@
+a=123+4
+b = 987
+
+if a > 100:
+    print("true")
+else:
+    print("false")
